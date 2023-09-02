c9ab1566fa0d6266f31757d51f29b45209f5de26
Cedric Lim Ah Tock <c_limaht@live.concordia.ca>
2023-09-02 13:35:17 -0400

    Random

--- client/src/pages/Feedback.js
+++ client/src/pages/Feedback.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './style/Feedback.css';
 
 const Feedback = () => {
-  return (
-    <div>Support</div>
-  )
-}
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    dropdown: 'Feedback', // default value
+    message: ''
+  });
 
-export default Feedback
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className='form-container'>
+      <h1 className='form-title'>Feedback Form</h1>
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={form.name}
+          onChange={handleChange}
+          className='form-input'
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          name='email'
+          value={form.email}
+          onChange={handleChange}
+          className='form-input'
+        />
+        <select
+          className='form-dropdown'
+          name='dropdown'
+          value={form.dropdown}
+          onChange={handleChange}
+        >
+          <option disabled value=''>
+            Select an option
+          </option>
+          <option value='Feedback'>Feedback</option>
+          <option value='Report'>Report</option>
+          <option value='Inquiry'>Inquiry</option>
+          <option value='Other'>Other</option>
+        </select>
+        <textarea
+          placeholder='Message'
+          name='message'
+          value={form.message}
+          onChange={handleChange}
+          className='form-textarea'
+        />
+        <button type='submit' className='form-button'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Feedback;
