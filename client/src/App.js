ff9e21f66278b12a48bf13405943a2a89ac77aa6
Cedric Lim Ah Tock <c_limaht@live.concordia.ca>
2023-09-18 01:41:39 -0400

    Added proper access to timeline page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
@@ -12,6 +12,7 @@ import Timeline from './pages/Timeline';
 
 function App() {
   const user = localStorage.getItem("jwtToken");
+  
   return (
     <Router>
       <div>
@@ -19,10 +20,9 @@ function App() {
           <Route path="/" element={<Layout><Home /></Layout>} />
           <Route path="/explore" element={<Layout><Explore /></Layout>} />
           <Route path="/feedback" element={<Layout><Feedback /></Layout>} />
-          {user && <Route path="/timeline" element={<Layout><Timeline /></Layout>} />}
-          {/* Login without Layout */}
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={user ? <Navigate to="/timeline" /> : <Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/timeline' element={user ? <Layout><Timeline /></Layout> : <Navigate to="/login" />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
